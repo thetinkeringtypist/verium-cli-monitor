2ea7bff7f3fc30f41d26e62114e0306ff36e252c
bezeredi <bezeredi.dev@gmail.com>
2018-03-09 23:15:13 -0500

    monitor.py: updated commenting format due to complaints about the #!

--- monitor.py
+++ monitor.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 #
-#! Author: Bezeredi, Evan D.
+# Author: Bezeredi, Evan D.
 #
-#! CLI Monitor to display cpuminer information for each worker on the LAN
+# CLI Monitor to display cpuminer information for each worker on the LAN
 #
 #! NOTE: Requires a list of hostnames/IPs on the LAN in
 #        /home/<username>/.chosts
 #
-#!       Also requires miner-apid.py to running on each worker.
+#       Also requires miner-apid.py to running on each worker.
 #
-#! I have this script running on my LAN controller so that finding each machine
+# I have this script running on my LAN controller so that finding each machine
 #  on the LAN is not a problem
 import socket as pysocket
 import time
@@ -23,36 +23,36 @@ ports = [4048,4049]
 hosts_file_str = "{0}/.chosts".format(Path.home())
 
 
-#! Thread variables
+# Thread variables
 threads = []
 kill_threads = threading.Event()
 
-#! Display varaibles
+# Display varaibles
 stdscr = curses.initscr()
 windows = []
 hosts = []
 statinfo_list = []
 
 
-#! Initialize display variables
+# Initialize display variables
 def init_display():
 	(term_height, term_width) = stdscr.getmaxyx()
 
 	for host in hosts:
-		#! Only (offline, host) since no value will be accessed 
-		#  other than these if the host is offline
+		# Only (offline, host) since no value will be accessed 
+		# other than these if the host is offline
 		statinfo_list.append((False, host))
 
-	#! Header window (index 0)
+	# Header window (index 0)
 	windows.append(curses.newwin(3, term_width, 0, 0))
 
-	#! Hosts window (pad) (index 1)
-	if len(hosts) < (term_height - 7): #! Header and footer
+	# Hosts window (pad) (index 1)
+	if len(hosts) < (term_height - 7): # Header and footer
 		windows.append(curses.newpad(term_height - 7, term_width))
 	else:
 		windows.append(curses.newpad(len(hosts), term_width))
 		
-	#! Footer window (index 2)
+	# Footer window (index 2)
 	windows.append(curses.newwin(4, term_width, term_height - 4, 0))
 
 	if curses.has_colors():
@@ -73,19 +73,19 @@ def init_display():
 	return
 
 
-#! Define custom colors
+# Define custom colors
 def init_colors():
 	for i in range(0, curses.COLORS):
         	curses.init_pair(i + 1, i, -1)
 
-#! Interrupt signal handler
+# Interrupt signal handler
 def signal_handler(signal, frame):
 	kill_program()
 
 
-#! Kill program
+# Kill program
 def kill_program():
-	#! Kill Threads
+	# Kill Threads
 	kill_threads.set()
 	print("Waiting for background threads...")
 	for t in threads:
@@ -95,7 +95,7 @@ def kill_program():
 	return
 
 
-#! Process messages from a zmqsocket
+# Process messages from a zmqsocket
 def process_worker_msg(host):
 	miner_results = []
 	buffer_size = 4096
@@ -104,16 +104,16 @@ def process_worker_msg(host):
 		miner_results.clear()
 		time.sleep(1)
 
-		#! For each possible port in use
+		# For each possible port in use
 		for port in ports:
 			socket = pysocket.socket(pysocket.AF_INET, pysocket.SOCK_STREAM)
 			try:
-				#! Request miner information
+				# Request miner information
 				socket.connect((host, port))
 				socket.settimeout(5)
 				socket.send("summary".encode())
 
-				#! Receive miner information
+				# Receive miner information
 				socket.settimeout(5)
 				msg = socket.recv(buffer_size).decode()
 				miner_results.append(parse_summary_msg(host,msg))
@@ -126,18 +126,18 @@ def process_worker_msg(host):
 	return
 
 
-#! Change display to reflect that the host is offline
+# Change display to reflect that the host is offline
 def set_host_offline(host):
 	index = hosts.index(host)
 	statinfo_list[index] = (False, host)
 	return
 
 
-#! Parse message received from server
+# Parse message received from server
 def parse_summary_msg(host, msg):
 	data_points = msg.split('|')[0].split(';')
 
-	#! Get values
+	# Get values
 	name     = data_points[0].split('=')[1]
 	version  = data_points[1].split('=')[1]
 	api      = data_points[2].split('=')[1]
@@ -152,25 +152,25 @@ def parse_summary_msg(host, msg):
 	cpu_temp = float(data_points[11].split('=')[1])
 	cpu_fan  = int(data_points[12].split('=')[1])
 	cpu_freq = int(data_points[13].split('=')[1])
-	uptime   = int(data_points[14].split('=')[1])   #! Uptime is in seconds
-	time_sec = int(data_points[15].split('=')[1])   #! Time is in seconds
+	uptime   = int(data_points[14].split('=')[1])   # Uptime is in seconds
+	time_sec = int(data_points[15].split('=')[1])   # Time is in seconds
 
-	#! Calculate hpm
+	# Calculate hpm
 	hpm     = khps * 1000 * 60
 	total = accepted + rejected if accepted + rejected > 0 else 1
 	percent = accepted / (total) * 100
 
-	#! Build the display string entry
-	#! (online, host, hpm, percent, blocks, difficulty, cpus, temp)
+	# Build the display string entry
+	# (online, host, hpm, percent, blocks, difficulty, cpus, temp)
 	return (True, host, hpm, percent, solved, diff, cpus, cpu_temp)
 
 
-#! Combine results from each miner status
+# Combine results from each miner status
 def combine_results(host, miner_results):
 	index = hosts.index(host)
 	count = len(miner_results)
 
-	#! Edge cases
+	# Edge cases
 	if count == 0:
 		set_host_offline(host)
 		return
@@ -178,7 +178,7 @@ def combine_results(host, miner_results):
 		statinfo_list[index] = miner_results[0]
 		return
 
-	#! Combine results from all miners on a worker
+	# Combine results from all miners on a worker
 	hpm = 0
 	solved = 0
 	percent = 0.0
@@ -196,14 +196,14 @@ def combine_results(host, miner_results):
 		if result[7] > cpu_temp:
 			cpu_temp = result[7]
 
-	#! Build the display string entry
-	percent /= count if count > 0 else 1  #! Normalize percent
+	# Build the display string entry
+	percent /= count if count > 0 else 1  # Normalize percent
 	statinfo_list[index] = (
 		miner_results[0][0], host, hpm, percent, solved, diff, cpus, cpu_temp)
 	return
 
 
-#! Calc totals and averages
+# Calc totals and averages
 def get_totals_avgs():
 	total_hashrate = 0.0
 	total_solved_blocks = 0
@@ -211,12 +211,12 @@ def get_totals_avgs():
 	online_hosts = list(filter(lambda info: info[0] == True, statinfo_list))
 	length = len(online_hosts) if len(online_hosts) > 0 else 1
 
-	#! Calculate totals
+	# Calculate totals
 	total_hashrate      = sum(i for _,_,i,_,_,_,_,_ in online_hosts)
 	total_solved_blocks = sum(i for _,_,_,_,i,_,_,_ in online_hosts)
 	total_cpus          = sum(i for _,_,_,_,_,_,i,_ in online_hosts)
 
-	#! Calculate averages
+	# Calculate averages
 	avg_hashrate = total_hashrate / length
 	avg_share_percent   = sum(i for _,_,_,i,_,_,_,_ in online_hosts) / length
 	avg_solved_blocks   = total_solved_blocks / length
@@ -224,13 +224,13 @@ def get_totals_avgs():
 	avg_cpus            = total_cpus / length
 	avg_cpu_temp        = sum(i for _,_,_,_,_,_,_,i in online_hosts) / length
 
-	#! Formulate Average String
+	# Formulate Average String
 	avg_str = ("Average {0:>18.3f} H/m   {1:>6.2f}%   {2:>6}    {3:<8f}  "
 		"│ {4:>4.2f}   {5:>5.1f}°C".format(
 		avg_hashrate,avg_share_percent,avg_solved_blocks,
 		avg_difficulty,avg_cpus,avg_cpu_temp))
 	
-	#! Formulate Average String
+	# Formulate Average String
 	total_str = ("Total   {0:>18.3f} H/m   ---.--%   {1:>6}    -.------  "
 		"│ {2:>4}   ---.-°C".format(
 		total_hashrate,total_solved_blocks,total_cpus))
@@ -238,9 +238,9 @@ def get_totals_avgs():
 	return (total_str, avg_str)
 	
 
-#! The display and user input loop
+# The display and user input loop
 def run_display_user_input():
-	#! Window info
+	# Window info
 	header_win = windows[0]
 	hosts_win = windows[1]
 	footer_win = windows[2]
@@ -255,7 +255,7 @@ def run_display_user_input():
 	hl_host = 0
 	start_y = 0
 
-	#! Print header information
+	# Print header information
 	header_win.addstr(0,0,      "  ┌─────────────────┬──────────────┬─────────┬────────┬────────────┬──────┬─────────┐")
 	header_win.clrtoeol()
 	header_win.addstr(1,0,      "  │   Hostname/IP   │ Hashrate H/m │ Share % │ Blocks │ Difficulty │ CPUs │ Temp °C │")
@@ -265,11 +265,11 @@ def run_display_user_input():
 	header_win.refresh()
 
 	while True:
-		#! Write hosts to screen and frame the scroll window
+		# Write hosts to screen and frame the scroll window
 		write_to_scr(hl_host)
 
-		#! Get user input
-		c = hosts_win.getch()  #! Calls stdscr.refresh()
+		# Get user input
+		c = hosts_win.getch()  # Calls stdscr.refresh()
 		if c == curses.KEY_DOWN:
 			if hl_host < (hosts_height - 1) and hl_host < (hosts_len - 1):
 				hl_host += 1
@@ -287,27 +287,27 @@ def run_display_user_input():
 			hl_host = hosts_height - 1
 			start_y = hl_host - hosts_scroll_max
 		elif c == ord('q') or c == 27:
-			#! Either q or ESC to quit
+			# Either q or ESC to quit
 			break
 		else:
-			pass #! Leave everything as is
+			pass # Leave everything as is
 
 		hosts_win.refresh( start_y,0, 3,0, footer_start,term_width)
 		footer_win.refresh()
 
-		#! Negligable refresh lag while
-		#  keeping CPU usage down
-		#  (for arrowing up and down)
-		#! I want to use a keyboard event
-		#  listener, but I can't find an
-		#  implementation that works over
-		#  ssh
+		# Negligable refresh lag while
+		# keeping CPU usage down
+		# (for arrowing up and down)
+		# I want to use a keyboard event
+		# listener, but I can't find an
+		# implementation that works over
+		# ssh
 		time.sleep(0.03)
 
 	return
 
 
-#! Write strings to the screen
+# Write strings to the screen
 def write_to_scr(hl_host):
 	hosts_win = windows[1]
 	footer_win = windows[2]
@@ -315,17 +315,17 @@ def write_to_scr(hl_host):
 	
 	i = 0
 	for statinfo in statinfo_list:
-		#! Highlight host
+		# Highlight host
 		hl = (True if i == hl_host else False)
 		apply_formatting(i, statinfo, hl)
 		i += 1
 		
-	#! Print empty lines to fill the terminal
+	# Print empty lines to fill the terminal
 	for b in range(i, hosts_height):
 		hosts_win.addstr(b,0,"│ │                                                                │                │")
 		hosts_win.clrtoeol()
 
-	#! Calculate totals and averages
+	# Calculate totals and averages
 	(total_str,avg_str) = get_totals_avgs()
 	footer_win.addstr(0,0, "├─┼────────────────────────────────────────────────────────────────┼────────────────┤")
 	footer_win.clrtoeol()
@@ -339,83 +339,83 @@ def write_to_scr(hl_host):
 	return
 
 
-#! Applies formatting and coloring for written lines
+# Applies formatting and coloring for written lines
 def apply_formatting(line, statinfo, hl):
 	hosts_win = windows[1]
 
 	hl_prefix = "│>│"
 	prefix =    "│ │"
 
-	#! Host online, highlighted
+	# Host online, highlighted
 	if statinfo[0] == True and hl == True:
 		hosts_win.addstr(line, 0, hl_prefix)
-		#! Three spaces between each. Space, bar, space between diff and cpus
+		# Three spaces between each. Space, bar, space between diff and cpus
 		hosts_win.addstr(" {0:<15}   ".format(statinfo[1]), curses.A_REVERSE)
-		hosts_win.addstr("{0:>8.3f} H/m".format(statinfo[2]), curses.A_REVERSE) #! HPM
+		hosts_win.addstr("{0:>8.3f} H/m".format(statinfo[2]), curses.A_REVERSE) # HPM
 		hosts_win.addstr("   ", curses.A_REVERSE)
-		hosts_win.addstr("{0:>6.2f}%".format(statinfo[3]), curses.A_REVERSE)   #! Share %
+		hosts_win.addstr("{0:>6.2f}%".format(statinfo[3]), curses.A_REVERSE)   # Share %
 		hosts_win.addstr("   {0:>6}    {1:<8}  │ {2:>4}   ".format(
 			statinfo[4], statinfo[5], statinfo[6]), curses.A_REVERSE)
-		hosts_win.addstr("{0:>5.1f}°C ".format(statinfo[7]), curses.A_REVERSE)  #! CPU Temp
+		hosts_win.addstr("{0:>5.1f}°C ".format(statinfo[7]), curses.A_REVERSE)  # CPU Temp
 
-	#! Host online, not highlighted
+	# Host online, not highlighted
 	elif statinfo[0] == True and hl == False:
 		hosts_win.addstr(line, 0, prefix)
 		hosts_win.addstr(" {0:<15}   ".format(statinfo[1]))
-		hosts_win.addstr("{0:>8.3f} H/m".format(statinfo[2])) #! HPM
+		hosts_win.addstr("{0:>8.3f} H/m".format(statinfo[2])) # HPM
 		hosts_win.addstr("   ")
-		hosts_win.addstr("{0:>6.2f}%".format(statinfo[3]))   #! Share %
+		hosts_win.addstr("{0:>6.2f}%".format(statinfo[3]))   # Share %
 		hosts_win.addstr("   {0:>6}    {1:<8}  │ {2:>4}   ".format(
 			statinfo[4], statinfo[5], statinfo[6]))
-		hosts_win.addstr("{0:>5.1f}°C ".format(statinfo[7]))  #! CPU Temp
+		hosts_win.addstr("{0:>5.1f}°C ".format(statinfo[7]))  # CPU Temp
 		
-	#! Host offline, highlighted
+	# Host offline, highlighted
 	elif statinfo[0] == False and hl == True:
 		hosts_win.addstr(line, 0, hl_prefix)
 		hosts_win.addstr(" {0:<15}    ----.-- H/m   ---.--%   ------    -.------  │ ----   ---.-°C ".format(statinfo[1]), curses.A_REVERSE)
 
-	#! host offline, non-highlighted
+	# host offline, non-highlighted
 	else:
 		hosts_win.addstr(line, 0, prefix)
 		hosts_win.addstr(" {0:<15}    ----.-- H/m   ---.--%   ------    -.------  │ ----   ---.-°C ".format(statinfo[1]))
 		
 	
-	#! End of Line
+	# End of Line
 	hosts_win.addstr("│")
 	hosts_win.clrtoeol()
 
 	return
 
 
-#! Main function
+# Main function
 def main(stdscr):
 	kill_threads.clear()
 
-	#! Create list of hosts
+	# Create list of hosts
 	Path(hosts_file_str).touch(exist_ok=True)
 	hosts_file = open(hosts_file_str,'r')
 	for line in hosts_file:
 		hosts.append(line.rstrip())
 	hosts_file.close()
 
-	#! Initialize
+	# Initialize
 	init_display()
 	signal.signal(signal.SIGINT, signal_handler)
 
-	#! Create threads and start
+	# Create threads and start
 	for host in hosts:
 		t = threading.Thread(target=process_worker_msg, args=(host,))
 		threads.append(t)
 		t.start()
 
-	#! Run display and user input loop
+	# Run display and user input loop
 	run_display_user_input()
 
-	#! Kill the program
+	# Kill the program
 	return
 
 
-#! Run the program
+# Run the program
 if __name__ == "__main__":
 	curses.wrapper(main)
 	kill_program()
